12aeffa23e504f2722f23910acca100e39c17ef9
adi forkasiewicz <adiforka@gmail.com>
2020-07-07 10:57:38 +0200

    and refactored back, since we have validator service validate every argument separately anyway, at method signature level, and I was giving every argument a list of annotations from all the arguments. might get tricky if different were to be thrown for different arguments' violations in the same target method signature.

--- src/main/java/com/adison/shop/common/validator/ModelValidator.java
+++ src/main/java/com/adison/shop/common/validator/ModelValidator.java
@@ -18,34 +18,26 @@ public class ModelValidator {
 
     private final ValidatorService validatorService;
 
-    //return type: any, method name: any, but arguments must be annotated with validate, whatever type they may be
-    //check this: without a space after "@Validate" it gives us a naming error
-    //execution(* *(@Validate (*)))
     @Before("execution(* *(@Validate (*)))")
     public void validate(JoinPoint joinPoint) {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         Object[] args = joinPoint.getArgs();
         IntStream.range(0, args.length)
-                .forEach(index -> validate(methodSignature, args[index]));
+                .forEach(index -> validate(methodSignature, args[index], index));
     }
 
-    //we do a lot of work on this universal model validator to make sure we only validate the arguments to a method
-    //that have been annotated with out Custom validation annotation
-    private void validate(MethodSignature methodSignature, Object argument) {
-        Annotation[][] annotations = getAnnotations(methodSignature);
+    private void validate(MethodSignature methodSignature, Object argument, int index) {
+        Annotation[] annotations = getAnnotations(methodSignature, index);
         Optional<Validate> optionalValidateAnno = getValidateAnnotation(annotations);
         optionalValidateAnno.ifPresent(validate -> validatorService.validate(argument, validate.exception()));
     }
 
-    private Annotation[][] getAnnotations(MethodSignature methodSignature) {
-        //each argument may be annotated with +1 annotations, so we get an array per argument
-        //and a matrix all in all, since there may be more than one argument
-        return methodSignature.getMethod().getParameterAnnotations();
+    private Annotation[] getAnnotations(MethodSignature methodSignature, int argIndex) {
+        return methodSignature.getMethod().getParameterAnnotations()[argIndex];
     }
 
-    private Optional<Validate> getValidateAnnotation(Annotation[][] annotations) {
+    private Optional<Validate> getValidateAnnotation(Annotation[] annotations) {
         return Arrays.stream(annotations)
-                .flatMap(Stream::of)
                 .filter(annotation -> annotation instanceof Validate)
                 .map(annotation -> (Validate) annotation)
                 .findFirst();
