c76412ab1cbf3c814369b0ddd46cfc0742fdb331
adi forkasiewicz <adiforka@gmail.com>
2020-07-27 21:51:22 +0200

    added add-user.jsp with a user form from Spring. added a POST to in UserWebController to add a user, with a redirect to show-users once added

--- src/main/java/com/adison/shop/users/UserWebController.java
+++ src/main/java/com/adison/shop/users/UserWebController.java
@@ -5,6 +5,7 @@ import com.adison.shop.common.web.PagedResultDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -14,6 +15,24 @@ public class UserWebController {
 
     private final UserService userService;
     private final UserMapper userMapper;
+
+    //pattern with forms: get (gets form), post (posts form data), redirect (gets users, e.g., with the new data included)
+
+    //this is to return a form view with a blank user object to populate through the sf form
+    @GetMapping("add-user.html")
+    public ModelAndView addUser() {
+        ModelAndView mv = new ModelAndView("add-user");
+        //if I don't add a key, the object will be identified by type, lower-case ("user"). add for binding
+        mv.addObject(new User());
+        return mv;
+    }
+
+    //this is to add the user (Spring will map the form data in the view to the User object and inject it into the method
+    @PostMapping("add-user.html")
+    public String saveUser(User user) {
+        userService.add(user);
+        return "redirect:show-users.html";
+    }
 
     @GetMapping("show-users.html")
     public ModelAndView showUsers(
