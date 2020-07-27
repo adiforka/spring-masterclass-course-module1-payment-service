3848925fd6b8b5b61eec6432c6d05f7fdfc0c92e
adi forkasiewicz <adiforka@gmail.com>
2020-07-27 17:10:50 +0200

    added basic JSP (cannot find index.jsp in WEB-INF/views when I run app now)

--- src/main/java/com/adison/shop/MvcConfiguration.java
+++ src/main/java/com/adison/shop/MvcConfiguration.java
@@ -3,9 +3,7 @@ package com.adison.shop;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -15,10 +13,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @ComponentScan(basePackages = "com.adison.shop")
 @EnableWebMvc
 @Configuration
-//WebMvc has lots of default methods we can use (or override)
 public class MvcConfiguration implements WebMvcConfigurer {
 
-    //getting swagger to register our controllers (scanning base packages)
     @Bean
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -27,9 +23,6 @@ public class MvcConfiguration implements WebMvcConfigurer {
                 .build();
     }
 
-    //set up for static resources of Swagger's that we want sent to the client, with the DispatcherServlet not handling the
-    //request through its usual cycle. Swagger documentation lists resources that should be made accessible this way
-    //this is something SB does by default
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
@@ -38,4 +31,18 @@ public class MvcConfiguration implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
 
     }
+
+    //resolvers put together and return a view from a logical view name received from the controller (thru a ds)
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+
+    //don't have to write controllers that have no logic except returning a view directly--just have to register them
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("index.html").setViewName("index");
+    }
+
 }
---
+++ src/main/webapp/WEB-INF/views/menu.jsp
@@ -0,0 +1,5 @@
+<ul class="nav nav-pills">
+    <li class="nav-item">
+        <a class="nav-link" href="add-user.html"><s:message code="index.addUserLink"/></a>
+    </li>
+</ul>
