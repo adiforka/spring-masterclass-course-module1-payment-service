7853ca63e970cb39df81caf5446391c7c01a608c
adi forkasiewicz <adiforka@gmail.com>
2020-08-21 15:47:46 +0200

    added a @Scheduled annotated method for logging number of currently placed orders in OrderService

--- src/main/java/com/adison/shop/orders/OrderService.java
+++ src/main/java/com/adison/shop/orders/OrderService.java
@@ -4,17 +4,21 @@ import com.adison.shop.common.PagedResult;
 import com.adison.shop.common.validator.Validate;
 import com.adison.shop.payments.Payment;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.mail.internet.MimeMessage;
 import javax.transaction.Transactional;
 import java.time.Instant;
 import java.util.UUID;
+import java.util.logging.Level;
 
 @Transactional
+@Log
 @RequiredArgsConstructor
 public class OrderService {
 
@@ -38,11 +42,16 @@ public class OrderService {
                 .orElseThrow(OrderNotFoundException::new);
     }
 
+    @Scheduled(cron = "*/10 * * * * *")
+    public void printSummary() {
+        log.log(Level.INFO, "Placed orders " + orderRepository.count());
+    }
+
     private void sendEmail() {
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             var messageHelper = new MimeMessageHelper(mimeMessage);
             messageHelper.setFrom("adiforka@gmail.com");
-            messageHelper.setTo("the.adicide@gmail.com");
+            messageHelper.setTo("adiforka@gmail.com");
             messageHelper.setSubject("New order");
             messageHelper.setText("New order has been placed", true);
         };
